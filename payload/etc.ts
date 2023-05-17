5607dfb542f5db8e681a2b191459fe62d7561cb7
Shoupeach <shoutrawberry@gmail.com>
2023-05-17 23:38:28 +0900

    🫐 docs: Set etc

--- payload/etc.ts
+++ payload/etc.ts
@@ -5,20 +5,21 @@ const etc: IEtc.Payload = {
 
   list: [
     {
-      title: 'Capstone Design Contest',
-      subTitle: 'Grand Prize',
-      startedAt: '2015-05',
+      title: '알테스터디 1기',
+      subTitle: '알고리즘 테스트를 위한 알고리즘 스터디',
+      startedAt: '2023-04',
     },
     {
-      title: 'IT Education Service Activities',
-      subTitle: 'Wrold Friends IT Volunteer',
-      startedAt: '2013-07',
-      endedAt: '2013-08',
+      title: '전북대학교 사회봉사',
+      subTitle: '필리핀 바기오 해외봉사',
+      startedAt: '2018-06',
+      endedAt: '2018-07',
     },
     {
-      title: 'Information Olympiad',
-      subTitle: 'Silver Prize',
-      startedAt: '2008-05',
+      title: '전북대학교 오프캠퍼스집중실용영어',
+      subTitle: '필리핀 코딜레라대학교 어학연수',
+      startedAt: '2016-07',
+      endedAt: '2016-08',
     },
   ],
 };
