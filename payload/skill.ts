33b3c2ac568882abdc26565bff4d26bdd46d3498
Shoupeach <shoutrawberry@gmail.com>
2023-05-17 22:23:03 +0900

    🫐 docs: Set skill

--- payload/skill.ts
+++ payload/skill.ts
@@ -1,75 +1,22 @@
 import { ISkill } from '../component/skill/ISkill';
 
-const backend: ISkill.Skill = {
-  category: 'Back-end',
-  items: [
-    {
-      title: 'Node.js',
-      level: 3,
-    },
-    {
-      title: 'AWS',
-      level: 3,
-    },
-    {
-      title: 'PHP',
-      level: 2,
-    },
-    {
-      title: 'Java',
-      level: 1,
-    },
-    {
-      title: 'Python',
-      level: 1,
-    },
-    {
-      title: 'C/C++',
-      level: 1,
-    },
-  ],
-};
-
-const database: ISkill.Skill = {
-  category: 'Database',
-  items: [
-    {
-      title: 'MySQL',
-      level: 3,
-    },
-    {
-      title: 'Oracle',
-      level: 2,
-    },
-    {
-      title: 'Redis',
-      level: 2,
-    },
-    {
-      title: 'MongoDB',
-      level: 1,
-    },
-  ],
-};
-
 const frontend: ISkill.Skill = {
   category: 'Front-end',
   items: [
     {
-      title: 'Next.js',
-      level: 2,
+      title: 'JavaScript',
+    },
+    {
+      title: 'TypeScript',
     },
     {
       title: 'React.js',
-      level: 2,
     },
     {
-      title: 'javascript',
-      level: 2,
+      title: 'Next.js',
     },
     {
       title: 'HTML/CSS',
-      level: 2,
     },
   ],
 };
@@ -78,30 +25,21 @@ const etc: ISkill.Skill = {
   category: 'Etc',
   items: [
     {
-      title: 'Ubuntu',
-    },
-    {
-      title: 'Vim',
-    },
-    {
-      title: 'Security',
-    },
-    {
       title: 'VS Code',
     },
     {
-      title: 'Jira',
+      title: 'Notion',
     },
     {
-      title: 'Confluence',
+      title: 'Figma',
     },
   ],
 };
 
 const skill: ISkill.Payload = {
   disable: false,
-  skills: [backend, database, frontend, etc],
-  tooltip: '1: 기초 수준\n2: 취미 개발 수준\n3: Production 개발 가능 수준',
+  skills: [frontend, etc],
+  tooltip: '',
 };
 
 export default skill;
