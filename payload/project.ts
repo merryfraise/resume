516914519a4743de1eb0efcf4d49ee4c8b992f82
Shoupeach <shoutrawberry@gmail.com>
2023-05-17 22:48:25 +0900

    🫐 docs: Set project

--- payload/project.ts
+++ payload/project.ts
@@ -4,41 +4,42 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Mashup API Development',
-      startedAt: '2018-02',
-      where: 'Foo Company',
+      title: '겜투(GAMETO) : 게이머를 위한 소셜 서비스',
+      startedAt: '2023-01',
+      endedAt: '2023-02',
+      where: '코드스테이츠 메인 프로젝트 (6인 /4주)',
       descriptions: [
-        { content: 'Development and operation of Mashup API of Aggregation Layer' },
+        { content: 'React.js | Redux Toolkit | Styled-Components | Axios', weight: 'BOLD' },
+        { content: '게임의 파티원 구인 시스템과 SNS을 결합한 서비스 기획 및 프론트 개발' },
         {
-          content: 'Add Foo Feature in 2019',
-          weight: 'MEDIUM',
-          descriptions: [
-            { content: 'Integer ut libero eu libero mattis rutrum at at urna' },
-            { content: 'Integer non turpis ante' },
-          ],
+          content:
+            '재사용이 가능한 컴포넌트 설계로 UI/UX에 통일성을 부여함과 동시에 개발에 소요되는 시간을 단축함',
+          // descriptions: [
+          //   { content: 'Integer ut libero eu libero mattis rutrum at at urna' },
+          //   { content: 'Integer non turpis ante' },
+          // ],
         },
+        { content: '프로필, 프로필 수정, 차단 목록, 탈퇴 페이지 구현' },
+        { content: '서버의 게임 DB와 클라이언트의 게임 icon을 매치시키는 유틸 함수 제작' },
         {
-          content: 'Launched Bar Service in 2018',
-          weight: 'MEDIUM',
-          descriptions: [
-            { content: 'Suspendisse vestibulum odio id libero facilisis gravida' },
-            {
-              content: 'In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus',
-            },
-          ],
+          content:
+            'Redux Toolkit을 이용한 전역 상태관리로 상태 업데이트에 따른 즉각적인 렌더링 구현',
         },
       ],
     },
     {
-      title: 'Lorem ipsum Project',
-      startedAt: '2016-10',
-      endedAt: '2017-11',
-      where: 'Bar Co., LTD.',
+      title: 'Stack Overflow : 클론 코딩',
+      startedAt: '2022-12',
+      endedAt: '2023-01',
+      where: '코드스테이츠 메인 프로젝트 (5인 /4주)',
       descriptions: [
-        { content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
-        { content: 'Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor' },
-        { content: 'Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien' },
-        { content: 'Proin bibendum non nibh eu aliquet' },
+        { content: 'React.js | Redux Toolkit | Styled-Components | Axios', weight: 'BOLD' },
+        { content: '로그인 비동기 처리 코드의 오류 수정' },
+        { content: '메인, 질문 작성, 질문 수정, 답변 수정, 유저 목록, 프로필 페이지 구현' },
+        {
+          content:
+            'Redux Toolkit을 이용한 전역 상태관리로 서치박스의 ON/OFF 상태 변화에 따른 렌더링 구현',
+        },
       ],
     },
   ],
