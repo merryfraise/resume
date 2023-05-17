d1ba9828833b8c99d1a25e4ec895f20fc92f4e0c
Shoupeach <shoutrawberry@gmail.com>
2023-05-17 23:56:03 +0900

    🫐 docs: Update introduce

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -5,8 +5,9 @@ const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor. Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien. Proin bibendum non nibh eu aliquet. In sit amet arcu sed nisi tincidunt ultricies. Fusce magna tellus, egestas at pretium quis, rutrum et ante. Phasellus efficitur lectus felis, sed lacinia enim rhoncus ut. Sed id mi vitae neque aliquet porttitor. Fusce dignissim ipsum urna, at finibus lectus sodales ut. Nulla ut purus vestibulum, dapibus tellus consequat, porttitor purus. Nunc egestas tortor ut vehicula mollis.',
-    'Integer ut libero eu libero mattis rutrum at at urna. Integer non turpis ante. Suspendisse vestibulum odio id libero facilisis gravida. In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus. Quisque ornare eget ex ut congue. Nam vel finibus sem. Phasellus id neque nibh. Nullam turpis velit, pharetra vitae mi non, bibendum maximus urna.',
+    'JavaScript와 React.js를 기반으로 꾸준히 개발 공부를 하고 있는 신입 프론트엔드 개발자입니다.',
+    '학부 시절 여러 개발 언어에 대해 접하게 된 것을 시작으로 개발 세계에 입문하였습니다. 부트캠프를 통한 2회의 협업 프로젝트 경험이 있고, 개인적으로도 협업 프로젝트를 찾아 현재 장기 취미 프로젝트에 합류한 상태입니다.',
+    '배우고 적용하는 것을 좋아합니다. 부트캠프 수료 후에도 꾸준히 알고리즘, Next.js와 TypeScript에 대해 학습하고 기록하고 있습니다.',
   ],
   sign: 'Subin',
   // sign: packageJson.author.name,
